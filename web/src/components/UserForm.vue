c4f8c9afaf23ef3d13fcda06970522813bd0cb64
MGibran <maikgibranr@gmail.com>
2019-05-02 22:28:36 +0700

    Update form validation styling

--- web/src/components/UserForm.vue
+++ web/src/components/UserForm.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="columns is-paddingless is-vcentered is-multiline">
       <div class="column">
-        <b-field>
+        <b-field :type="isInvalid ? 'is-danger' : ''">
           <b-input type="text" v-model="username" placeholder="Username"></b-input>
         </b-field>
       </div>
 
       <div class="column">
-        <b-field>
+        <b-field :type="isInvalid ? 'is-danger' : ''">
           <b-input type="number" v-model="userlevel" placeholder="Level"></b-input>
         </b-field>
       </div>
 
       <div class="column">
-        <b-button type="is-success is-fullwidth">Create</b-button>
+        <b-button type="is-success is-fullwidth" @click="saveSheet()">Save</b-button>
       </div>
     </div>
 </template>
@@ -21,8 +21,19 @@
 <script>
 export default {
   data: () => ({
-    username: "",
-    userlevel: ''
-  })
+    username: '',
+    userlevel: '',
+    isInvalid: false
+  }),
+  methods: {
+    saveSheet: function() {
+      if (this.username === '' || this.userlevel === '') {
+        this.isInvalid = true
+        return
+      }
+
+      this.isInvalid = false
+    }
+  }
 };
 </script>
